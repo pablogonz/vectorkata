4fe8095757b13f6502364787b321c078a3856433
PABLO GONZALEZ TAVAREZ <1094765@est.intec.edu.do>
2021-12-06 22:05:31 -0400

    version 1.0.0

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectorkata
+{
+    public class vectorops
+    {
+        public double u { get; set; }
+        public double v { get; set; }
+
+        public vectorops(double x, double y)
+        {
+            this.u = x;
+            this.v = y;
+        }
+            public double Magnitudeop()
+            {
+                double pit = Math.Pow(this.u, 2) + Math.Pow(this.v, 2);
+                double total = Math.Sqrt(pit);
+
+
+                return pit;
+
+            }
+        
+       
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
